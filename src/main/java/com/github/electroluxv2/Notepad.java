30c2836cf162e0610eba93c72123711b76ea9ffd
ElectroluxV2 <electroluxv2@gmail.com>
2021-06-09 15:56:57 +0200

    Fixed cancel on file open

--- src/main/java/com/github/electroluxv2/Notepad.java
+++ src/main/java/com/github/electroluxv2/Notepad.java
@@ -149,7 +149,13 @@ public class Notepad extends Application {
         final var fileChooser = new FileChooser();
         fileChooser.setTitle("Save as");
         fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("All Files", "*.*"));
-        view.save(fileChooser.showSaveDialog(scene.getWindow()));
+        final var file = fileChooser.showSaveDialog(scene.getWindow());
+
+        if (file == null) {
+            return null;
+        }
+
+        view.save(file);
         return null;
     }
 
